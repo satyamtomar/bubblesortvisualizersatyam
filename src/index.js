dd21576f19725a4d8ceee86bdeaa6b3a95fedae3
satyamtomar <satyamtomar@users.noreply.github.com>
2021-12-13 11:24:36 +0530

    update

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/*
+  **************
+  Satyam Tomar
+  saitamaitachi123@gmail.com
+  *************
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <>
+    <App />
+  </>,
+  document.getElementById('root')
+);
+
